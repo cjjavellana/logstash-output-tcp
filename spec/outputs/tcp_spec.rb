dcd67727775050c14293ee9188d7393a301bf8fe
Christian Javellana <cjjavellana@gmail.com>
2019-11-24 09:30:33 +0800

    [WIP] New: Allow multiple downstream tcp server

--- spec/outputs/tcp_spec.rb
+++ spec/outputs/tcp_spec.rb
@@ -1,31 +1,45 @@
-require "logstash/devutils/rspec/spec_helper"
-require "logstash/outputs/tcp"
-require "flores/pki"
+require 'logstash/devutils/rspec/spec_helper'
+require 'logstash/outputs/tcp'
+require 'flores/pki'
 
 describe LogStash::Outputs::Tcp do
   subject { described_class.new(config) }
-  let(:config) { {
-    "host" => "localhost",
-    "port" => 2000 + rand(3000),
-  } }
 
-  context "when enabling SSL" do
-    let(:config) { super.merge("ssl_enable" => true) }
-    context "and not providing a certificate/key pair" do
-      it "registers without error" do
+  let(:config) {
+    {
+      'host' => 'localhost',
+      'port' => 2000 + rand(3000),
+      'socket_address' => "localhost:#{2000 + rand(3000)}"
+    }
+  }
+
+  context 'when enabling SSL' do
+    let(:config) {
+      super.merge('ssl_enable' => true)
+    }
+
+    context 'and not providing a certificate/key pair' do
+      it 'registers without error' do
         expect { subject.register }.to_not raise_error
       end
     end
-    context "and providing a certificate/key pair" do
+
+    context 'and providing a certificate/key pair' do
       let(:cert_key_pair) { Flores::PKI.generate }
+
       let(:certificate) { cert_key_pair.first }
+
       let(:cert_file) do
-        path = Tempfile.new('foo').path
+        path = Tempfile.new.path
         IO.write(path, certificate.to_s)
         path
       end
-      let(:config) { super.merge("ssl_cert" => true, "ssl_cert" => cert_file) }
-      it "registers without error" do
+
+      let(:config) {
+        super.merge('ssl_enable' => true, 'ssl_cert' => cert_file)
+      }
+
+      it 'registers without error' do
         expect { subject.register }.to_not raise_error
       end
     end
